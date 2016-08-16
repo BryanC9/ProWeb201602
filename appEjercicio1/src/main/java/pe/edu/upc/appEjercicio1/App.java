a2ab890406be0cbc9a5327bc1047ae8fddb04114
BryanC9 <bryanchise9@gmail.com>
2016-08-16 09:29:11 -0500

    Modifique calculo de modulo I

---
+++ appEjercicio1/src/main/java/pe/edu/upc/appEjercicio1/App.java
@@ -0,0 +1,29 @@
+package pe.edu.upc.appEjercicio1;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Scanner s=new Scanner(System.in);
+    	int numero = 0;
+    	
+    	//Wrapper (Clases envolventes) ==> Casting
+    	//Double valores = new Double(13);
+    	
+    	
+    	String[] arrNombres = new String[3];
+    	
+        System.out.println( "Hello World!" );
+        for (int i = 0; i < arrNombres.length; i++) {
+			System.out.println("Nombre"+(i+1));
+			arrNombres[i] = s.nextLine();
+		}
+    
+    }
+}
